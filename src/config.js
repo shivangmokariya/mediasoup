835650f6c9b12b7894c232ba011a4fc87e7a3ad9
developer.designer18 <developer.designer18@gmail.com>
2024-04-11 15:39:07 +0530

    config file changes

--- src/config.js
+++ src/config.js
@@ -1,112 +1,21 @@
-// const os = require('os')
-// const ifaces = os.networkInterfaces()
-
-// const getLocalIp = () => {
-//   let localIp = '127.0.0.1'
-//   Object.keys(ifaces).forEach((ifname) => {
-//     for (const iface of ifaces[ifname]) {
-//       // Ignore IPv6 and 127.0.0.1
-//       if (iface.family !== 'IPv4' || iface.internal !== false) {
-//         continue
-//       }
-//       // Set the local ip to the first IPv4 address found and exit the loop
-//       localIp = iface.address
-//       return
-//     }
-//   })
-//   return localIp
-// }
-
-// module.exports = {
-//   listenIp: '0.0.0.0',
-//   listenPort: 3000,
-//   sslCrt: '../ssl/cert.pem',
-//   sslKey: '../ssl/key.pem',
-
-//   mediasoup: {
-//     // Worker settings
-//     numWorkers: Object.keys(os.cpus()).length,
-//     worker: {
-//       rtcMinPort: 10000,
-//       rtcMaxPort: 10100,
-//       logLevel: 'warn',
-//       logTags: [
-//         'info',
-//         'ice',
-//         'dtls',
-//         'rtp',
-//         'srtp',
-//         'rtcp'
-//         // 'rtx',
-//         // 'bwe',
-//         // 'score',
-//         // 'simulcast',
-//         // 'svc'
-//       ]
-//     },
-//     // Router settings
-//     router: {
-//       mediaCodecs: [
-//         {
-//           kind: 'audio',
-//           mimeType: 'audio/opus',
-//           clockRate: 48000,
-//           channels: 2
-//         },
-//         {
-//           kind: 'video',
-//           mimeType: 'video/VP8',
-//           clockRate: 90000,
-//           parameters: {
-//             'x-google-start-bitrate': 1000
-//           }
-//         }
-//       ]
-//     },
-//     // WebRtcTransport settings
-//     webRtcTransport: {
-//       listenIps: [
-//         {
-//           ip: '0.0.0.0',
-//           announcedIp: getLocalIp() // replace by public IP address
-//         }
-//       ],
-//       maxIncomingBitrate: 1500000,
-//       initialAvailableOutgoingBitrate: 1000000
-//     }
-//   }
-// }
-const os = require('os');
-const axios = require('axios'); // Import Axios for making HTTP requests
-
-const ifaces = os.networkInterfaces();
-
-// Function to fetch public IP address
-const getPublicIp = async () => {
-  try {
-    const response = await axios.get('https://api.ipify.org?format=json');
-    return response.data.ip;
-  } catch (error) {
-    console.error('Error fetching public IP:', error);
-    return null;
-  }
-};
+const os = require('os')
+const ifaces = os.networkInterfaces()
 
 const getLocalIp = () => {
-  let localIp = '127.0.0.1';
+  let localIp = '127.0.0.1'
   Object.keys(ifaces).forEach((ifname) => {
     for (const iface of ifaces[ifname]) {
       // Ignore IPv6 and 127.0.0.1
       if (iface.family !== 'IPv4' || iface.internal !== false) {
-        continue;
+        continue
       }
       // Set the local ip to the first IPv4 address found and exit the loop
-      localIp = iface.address;
-      return;
+      localIp = iface.address
+      return
     }
-  });
-  return localIp;
-};
+  })
+  return localIp
+}
 
 module.exports = {
   listenIp: '0.0.0.0',
@@ -159,16 +68,11 @@ module.exports = {
       listenIps: [
         {
           ip: '0.0.0.0',
-          announcedIp: getLocalIp() // Initial value
+          announcedIp: getLocalIp() // replace by public IP address
         }
       ],
       maxIncomingBitrate: 1500000,
       initialAvailableOutgoingBitrate: 1000000
     }
   }
-};
-
-// Update announcedIp with public IP asynchronously
-getPublicIp().then((publicIp) => {
-  module.exports.mediasoup.webRtcTransport.listenIps[0].announcedIp = publicIp;
-});
+}
