42289e2519b14ca888696dda6fda2188a2668901
developer.designer18 <developer.designer18@gmail.com>
2024-04-30 16:00:05 +0530

    cpu config

--- src/config.js
+++ src/config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   mediasoup: {
     // Worker settings
-    numWorkers: Object.keys(os.cpus()).length,
+    numWorkers: 1,
     worker: {
       rtcMinPort: 10000,
       rtcMaxPort: 10100,
@@ -62,51 +62,51 @@ module.exports = {
 // Router settings
 router: {
   mediaCodecs: [
-      {
-          kind: 'audio',
-          mimeType: 'audio/opus',
-          clockRate: 48000,
-          channels: 2,
+    {
+      kind: 'audio',
+      mimeType: 'audio/opus',
+      clockRate: 48000,
+      channels: 2,
+    },
+    {
+      kind: 'video',
+      mimeType: 'video/VP8',
+      clockRate: 90000,
+      parameters: {
+        'x-google-start-bitrate': 2000, // Set the start bitrate to 2000 kbps
       },
-      {
-          kind: 'video',
-          mimeType: 'video/VP8',
-          clockRate: 90000,
-          parameters: {
-              'x-google-start-bitrate': 1000,
-          },
+    },
+    {
+      kind: 'video',
+      mimeType: 'video/VP9',
+      clockRate: 90000,
+      parameters: {
+        'profile-id': 2,
+        'x-google-start-bitrate': 2000, // Set the start bitrate to 2000 kbps
       },
-      {
-          kind: 'video',
-          mimeType: 'video/VP9',
-          clockRate: 90000,
-          parameters: {
-              'profile-id': 2,
-              'x-google-start-bitrate': 1000,
-          },
+    },
+    {
+      kind: 'video',
+      mimeType: 'video/h264',
+      clockRate: 90000,
+      parameters: {
+        'packetization-mode': 1,
+        'profile-level-id': '4d0032',
+        'level-asymmetry-allowed': 1,
+        'x-google-start-bitrate': 2000, // Set the start bitrate to 2000 kbps
       },
-      {
-          kind: 'video',
-          mimeType: 'video/h264',
-          clockRate: 90000,
-          parameters: {
-              'packetization-mode': 1,
-              'profile-level-id': '4d0032',
-              'level-asymmetry-allowed': 1,
-              'x-google-start-bitrate': 1000,
-          },
+    },
+    {
+      kind: 'video',
+      mimeType: 'video/h264',
+      clockRate: 90000,
+      parameters: {
+        'packetization-mode': 1,
+        'profile-level-id': '42e01f',
+        'level-asymmetry-allowed': 1,
+        'x-google-start-bitrate': 2000, // Set the start bitrate to 2000 kbps
       },
-      {
-          kind: 'video',
-          mimeType: 'video/h264',
-          clockRate: 90000,
-          parameters: {
-              'packetization-mode': 1,
-              'profile-level-id': '42e01f',
-              'level-asymmetry-allowed': 1,
-              'x-google-start-bitrate': 1000,
-          },
-      },
+    },
   ],
 },
 
